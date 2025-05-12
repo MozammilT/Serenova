961841f3b903b2106a8335901e5e88476ee6a454
Mozammil <mozammiltarique007@gmail.com>
2025-05-13 01:15:23 +0530

    add initial files with frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-primary text-8xl tracking-tighter text-balance">
+        Hello React
+      </div>
+      <div className="text-secondary text-8xl tracking-tighter text-balance">
+        Hello Vite
+      </div>
+    </>
+  );
+}
+
+export default App;
