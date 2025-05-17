e828468e28b0d31064f87f31bb6a8e6fe7a291f7
Mozammil <mozammiltarique007@gmail.com>
2025-05-17 17:29:12 +0530

    Add Hero Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import "./styles/App.css";
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import Homepage from "./pages/Home.jsx";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <>
       {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+function Homepage() {
+  return (
+    <>
+      <Hero /> 
+      {/* time-stamp: 48.05 */}
+    </>
+  );
+}
+
+export default Homepage;
