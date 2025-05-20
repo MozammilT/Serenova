28365d6568b4d3d640c562da990057289203d734
Mozammil <mozammiltarique007@gmail.com>
2025-05-20 21:10:41 +0530

    Add Exclusive Offers Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Featuredest from "../components/FeatureDest";
+import ExclusiveOffers from "../components/ExclusiveOffers";
 
 function Homepage() {
   return (
     <>
       <Hero />
       <Featuredest />
+      <ExclusiveOffers />
     </>
   );
 }
