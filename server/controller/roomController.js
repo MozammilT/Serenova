e4513b4ba80f4d1efff5cb6893bd4f40497a87b9
Mozammil <mozammiltarique007@gmail.com>
2025-05-29 23:42:44 +0530

    Refactor room controller and routes to enhance functionality and error handling

--- server/controller/roomController.js
+++ server/controller/roomController.js
@@ -3,7 +3,6 @@ import Room from "../models/room.js";
 import { v2 as cloudinary } from "cloudinary";
 
 // API to create a new Room for Hotel
-
 export const createRoom = async (req, res) => {
   try {
     const { roomType, pricePerNight, amenities } = req.body;
@@ -30,19 +29,57 @@ export const createRoom = async (req, res) => {
     });
     res.json({ success: true, message: "Room created successfully" });
   } catch (err) {
-    console.log("Error in createRoom function: ", err);
-    res.json({success: false, message: err.message});
+    console.error("Error in createRoom function: ", err);
+    res.json({ success: false, message: err.message });
   }
 };
 
 // API to get all Room
-
-export const getRoom = async () => {};
+export const getRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find({ isAvailable: true })
+      .populate({
+        path: "hotel",
+        populate: {
+          path: "owner",
+          select: "image",
+        },
+      })
+      .sort({ createdAt: -1 });
+    res.json({ success: true, rooms });
+  } catch (err) {
+    console.error("Error in getRoom function:", err);
+    res.json({ success: false, message: err.message });
+  }
+};
 
 // API to get all Room for a specific Hotel
-
-export const getOwnerRoom = async () => {};
+export const getOwnerRoom = async (req, res) => {
+  try {
+    const hotelData = await Hotel({ owner: req.auth.userId });
+    const rooms = await Room.find({ hotel: hotelData._id.toString() }).populate(
+      "hotel"
+    );
+    res.json({ success: true, rooms });
+  } catch (err) {
+    console.error("Error in getOwnerRoom function:", err);
+    res.json({ success: false, message: err.message });
+  }
+};
 
 // API to toggle availability of a Room
-
-export const toggleRoomAvailability = async () => {};
+export const toggleRoomAvailability = async (req, res) => {
+  try {
+    const { roomId } = req.body;
+    const roomData = await Room.findById(roomId);
+    if (!roomData) {
+      return res.json({ success: false, message: "Room not found" });
+    }
+    roomData.isAvailable = !roomData.isAvailable;
+    await roomData.save();
+    res.json({ success: true, message: "Room availability updated" });
+  } catch (err) {
+    console.error("Error in toggleRoomAvailability function:", err);
+    res.json({ success: false, message: err.message });
+  }
+};
--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { createRoom } from "../controller/roomController.js";
+import {
+  createRoom,
+  getRooms,
+  getOwnerRoom,
+  toggleRoomAvailability,
+} from "../controller/roomController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post("/", upload.array("images", 4), protect, createRoom);
+roomRouter.get("/", getRooms);
+roomRouter.get("/owner", protect, getOwnerRoom);
+roomRouter.post("/toggle-availibility", protect, toggleRoomAvailability);
+
 export default roomRouter;
