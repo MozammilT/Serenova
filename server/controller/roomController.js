ce254ccdad471f7a3cda62f50de9d683f454805d
Mozammil <mozammiltarique007@gmail.com>
2025-05-29 18:12:54 +0530

    Add room schema and controller

---
+++ server/controller/roomController.js
@@ -0,0 +1,30 @@
+import Hotel from "../models/hotel.js"
+
+// API to create a new Room for Hotel
+
+export const createRoom = async() => {
+    try {
+
+    } catch (err) {
+
+    }
+}
+
+
+// API to get all Room
+
+export const getRoom = async() => {
+
+}
+
+// API to get all Room for a specific Hotel
+
+export const getOwnerRoom = async() => {
+
+}
+
+// API to toggle availability of a Room
+
+export const toggleRoomAvailability = async() => {
+
+}
