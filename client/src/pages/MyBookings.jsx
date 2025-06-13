3f806c9a1e63011a841682f37ab5c184d3386a11
Mozammil <mozammiltarique007@gmail.com>
2025-06-14 02:57:42 +0530

    Add delete booking function and loading state for the page

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import Title from "../components/Title";
-import { userBookingsDummyData } from "../constants/assets";
 import { toast } from "react-hot-toast";
 import { useAppContext } from "../context/AppContext.jsx";
 
 function MyBookings() {
   const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingBookingId, setLoadingBookingId] = useState(null);
   const { axios, getToken, user } = useAppContext();
 
   const fetchBookings = async () => {
     try {
+      setLoading(true);
       const { data } = await axios.get("/api/bookings/user", {
         headers: { Authorization: `Bearer ${await getToken()}` },
       });
@@ -21,6 +23,27 @@ function MyBookings() {
       }
     } catch (err) {
       toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteBooking = async (bookingId) => {
+    try {
+      setLoadingBookingId(bookingId);
+      const { data } = await axios.delete("/api/bookings/delete", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+        data: { bookingId },
+      });
+      if (data.success) {
+        fetchBookings();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingBookingId(null);
     }
   };
 
@@ -45,7 +68,11 @@ function MyBookings() {
           <div className="w-1/3">Payment</div>
         </div>
 
-        {bookings.length === 0 ? (
+        {loading ? (
+          <div className="flex justify-center items-center py-12">
+            <span className="inline-block w-10 h-10 border-4 border-gray-400 border-t-transparent rounded-full animate-spin"></span>
+          </div>
+        ) : bookings.length === 0 ? (
           <div className="text-center text-gray-500 py-12 text-lg">
             No bookings made yet.
           </div>
@@ -60,12 +87,14 @@ function MyBookings() {
                 <img
                   src={booking.room.images[0]}
                   alt="room-image"
-                  className="min-md:w-54 rounded shadow object-cover"
+                  className={`min-md:w-54 rounded shadow object-cover ${
+                    booking.status === "Cancelled" ? "grayscale" : ""
+                  }`}
                 />
                 <div className="flex flex-col gap-3 max-md:mt-3 min-md:ml-4">
-                  <p className="font-playfair text-2xl">
+                  <p className="font-playfair text-2xl flex flex-wrap gap-1 items-center">
                     {booking.hotel.name}
-                    <span className="font-inner text-sm ml-2">
+                    <span className="font-inner text-sm ml-2 whitespace-nowrap">
                       ({booking.room.roomType})
                     </span>
                   </p>
@@ -102,22 +131,63 @@ function MyBookings() {
                 <div className="flex items-center gap-2">
                   <div
                     className={`h-3 w-3 rounded-full ${
-                      booking.isPaid ? "bg-green-500" : "bg-red-500"
+                      booking.status === "Cancelled"
+                        ? "bg-gray-500"
+                        : booking.isPaid
+                        ? "bg-green-500"
+                        : "bg-red-500"
                     }`}
                   ></div>
                   <p
                     className={`h-3 w-3 rounded-full ${
-                      booking.isPaid ? "text-green-500" : "text-red-500"
+                      booking.status === "Cancelled"
+                        ? "text-gray-500"
+                        : booking.isPaid
+                        ? "text-green-500"
+                        : "text-red-500"
                     } mb-2.5`}
                   >
-                    {booking.isPaid ? "Paid" : "Unpaid"}
+                    {booking.status === "Cancelled"
+                      ? "Cancelled"
+                      : booking.isPaid
+                      ? "Paid"
+                      : "Unpaid"}
                   </p>
                 </div>
-                {!booking.isPaid && (
-                  <button className=" px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-900 hover:text-white transition-all cursor-pointer">
-                    Pay Now
-                  </button>
-                )}
+                <div className="flex gap-2">
+                  {!booking.isPaid && (
+                    <button
+                      className={`${
+                        booking.status === "Cancelled"
+                          ? "hidden"
+                          : "px-4 py-1.5 mt-4 text-sm border border-gray-400 rounded-full bg-gray-500 text-white hover:bg-gray-900 hover:text-white transition-all cursor-pointer"
+                      }`}
+                    >
+                      Pay Now
+                    </button>
+                  )}
+                  {!booking.isPaid && (
+                    <button
+                      disabled={loadingBookingId === booking._id}
+                      onClick={() => deleteBooking(booking._id)}
+                      className={`${
+                        booking.status === "Cancelled"
+                          ? "hidden"
+                          : "w-[87px] px-4 py-1.5 mt-4 text-sm border border-gray-400 rounded-full hover:bg-gray-900 hover:text-white transition-all flex items-center justify-center"
+                      } ${
+                        loadingBookingId === booking._id
+                          ? "cursor-not-allowed"
+                          : "cursor-pointer"
+                      }`}
+                    >
+                      {loadingBookingId === booking._id ? (
+                        <span className="inline-block w-5 h-5 border-[3px] border-gray-400 border-t-transparent rounded-full animate-spin"></span>
+                      ) : (
+                        "Cancel"
+                      )}
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           ))
