4a3d93c5ea74fc6268545964a883848de4902e6d
Mozammil <mozammiltarique007@gmail.com>
2025-06-18 23:36:58 +0530

    Add frontend URL to CORS whitelist

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://serenova-backend.vercel.app"], // Add your frontend URL
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://serenova-gamma.vercel.app"], // Add your frontend URL
     credentials: true,
   })
 );
