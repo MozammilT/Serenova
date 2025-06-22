d31d3d0418010283e21b25c2e1dfe5c902ea8001
Mozammil <mozammiltarique007@gmail.com>
2025-06-23 01:28:44 +0530

    Resolve Add Room button overlap issue on mobile

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -12,7 +12,7 @@ function Layout() {
     }
   }, [isOwner]);
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex h-full">
         <Sidebar />
